3c4fb3d62ebea621b14fa7082dca6286113aabf0
Sy Nguyen Nguyen <160448378+synguyen446@users.noreply.github.com>
2026-04-17 15:18:39 -0700

    added db connection to server.py

--- server.py
+++ server.py
@@ -1,4 +1,26 @@
 import socket
+import psycopg2
+
+# Replace with your Neon connection string
+DATABASE_URL = ""
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    cursor = conn.cursor()
+
+    print("Connected to Neon database!")
+
+    # Example query
+    cursor.execute("SELECT NOW();")
+    result = cursor.fetchone()
+    print("Current time from DB:", result)
+
+    cursor.close()
+    conn.close()
+
+except Exception as e:
+    print("Connection failed:", e)
+    
 
 def start_server():
     host = "0.0.0.0"   # listen on all available network interfaces
